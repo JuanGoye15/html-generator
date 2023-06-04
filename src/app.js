7b19ad7a2ff0c5920f3e1540406856713b57cb42
Juan Goyeneche <80859255+JuanGoye15@users.noreply.github.com>
2023-06-04 17:12:09 +0000

    Final version

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  myWebsite();
+};
+
+const myWebsite = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".net", ".us", ".io"];
+
+  let element = pronoun[Math.floor(Math.random() * pronoun.length)];
+  element += adj[Math.floor(Math.random() * adj.length)];
+  element += noun[Math.floor(Math.random() * noun.length)];
+  element += domain[Math.floor(Math.random() * domain.length)] + " ";
+  document.getElementById("website").innerHTML = element;
 };
